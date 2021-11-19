a81f34a9cacebd0f1de02301477652e5fee19199
JeonMinJae <ssi02014@naver.com>
2021-11-20 00:21:44 +0900

    Init(minjae): styled GlobalStyle

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app';
+import GlobalStyle from 'style/common';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
