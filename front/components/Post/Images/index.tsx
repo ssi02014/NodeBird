e8244ae639f776b3c87135ea4c7dab2557f3cc4d
JeonMinJae <ssi02014@naver.com>
2021-11-29 20:33:30 +0900

    Add(minjae): Images

--- front/components/Post/Images/index.tsx
+++ front/components/Post/Images/index.tsx
@@ -1,13 +1,76 @@
-import React from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import React, { useCallback, useMemo, useState } from 'react';
 import { ImageSrc } from 'redux/interface/post';
 
 interface Props {
   images: ImageSrc[];
 }
 const PostImages = ({ images }: Props) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+
+  }, []);
+
+  const renderImages = useMemo(() => {
+    if (images.length === 1) {
+      return (
+        <>
+          <img 
+            src={images[0].src} 
+            alt={images[0].src} 
+            onClick={onZoom}
+            role="presentation" // 클릭해도되지만 안해도 상관없음
+          />
+        </>
+      )
+    } else if (images.length === 2) {
+      return (
+        <>
+          <img 
+            width="50%"
+            src={images[0].src} 
+            alt={images[0].src} 
+            onClick={onZoom}
+            role="presentation" // 클릭해도되지만 안해도 상관없음
+          />
+          <img 
+            width="50%"
+            src={images[1].src} 
+            alt={images[1].src} 
+            onClick={onZoom}
+            role="presentation" // 클릭해도되지만 안해도 상관없음
+          />
+        </>
+      )
+    } else {
+      return (
+        <>
+          <img
+            width="50%"
+            src={images[0].src} 
+            alt={images[0].src} 
+            onClick={onZoom}
+            role="presentation" // 클릭해도되지만 안해도 상관없음
+          />
+          <div
+            role="presentation"
+            style={{ display: "inline-block", width: "50%", textAlign:"center", verticalAlign: "middle" }}
+            onClick={onZoom}
+          >
+            <PlusOutlined />
+            <br />
+            {images.length - 1}
+            개의 사진 더보기
+          </div>
+        </>
+      )
+    }
+  }, [images]);
+
   return (
     <div>
-      구현중...
+      {renderImages}
     </div>
   );
 };
--- front/redux/reducers/postReducer.ts
+++ front/redux/reducers/postReducer.ts
@@ -11,9 +11,8 @@ const initialState: PostState = {
       },
       content: "첫 번째 게시글 #해시태그, #익스프레스",
       Images: [
-        { src: "https://pixabay.com/get/g388d683d55cb95a354fd307f40e3966f3a384c3b303bf0a41b56fdf125ca6a5507fe75d07945a9393a5764f95e629281501007853a4c88c1a4e4cc459ed79d65bbbe9d09e0f9a8d930d4d4eb5ec869e0_1920.jpg" },
-        { src: "https://pixabay.com/get/g5ae934c107575cc860abd9ffffb0d6091575bb55a029b3a528c359d3480b412a47bae534d09e80a0e8915b2c21c35863d23c79ae5eeceadeeecda55de4a1a09b014aa4666029e8052669d85733f21872_1920.jpg" },
-        { src: "https://pixabay.com/get/g8df7c72b57c639473c3e18697382e2a83d2ca73b3add6cac4e0436219d354d0eb407a89a72974b4c16831338463b82427b6f7bf4d96f74cba96084928fc0e977_1920.jpg" },
+        { src: "https://i.ibb.co/jyBfpfh/bird-of-paradise-flower-6632515-1280.jpg" },
+        { src: "https://i.ibb.co/mzC8Ms9/cat-6576397-1280.jpg" },
       ],
       Comments: [
         {
